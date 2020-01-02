9c9cf6278f230f56329311aea80ae5a7a5104228
Naman Sood <54349762+nanusud@users.noreply.github.com>
2020-01-02 23:57:47 +0530

    Create test.js

---
+++ alphabetical-addition/test.js
@@ -0,0 +1,6 @@
+addLetters('a', 'b', 'c') = 'f'
+addLetters('a', 'b') = 'c'
+addLetters('z') = 'z'
+addLetters('z', 'a') = 'a'
+addLetters('y', 'c', 'b') = 'd' // notice the letters overflowing
+addLetters() = 'z'
