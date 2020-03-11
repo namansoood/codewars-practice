6653a975d3740281efa5e978a310733edca0eb5a
Naman Sood <54349762+nanusud@users.noreply.github.com>
2020-03-11 20:35:13 +0530

    Create test.js

---
+++ counting-sheep/test.js
@@ -0,0 +1,8 @@
+var array1 = [true,  true,  true,  false,
+              true,  true,  true,  true ,
+              true,  false, true,  false,
+              true,  false, false, true ,
+              true,  true,  true,  true ,
+              false, false, true,  true ];
+
+countSheeps(array1) //Should return 7
