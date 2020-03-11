b018809f66de5b4c6e95ac01496294542a42abe8
Naman Sood <54349762+nanusud@users.noreply.github.com>
2020-03-11 20:35:38 +0530

    fix: wrong expected answer

--- counting-sheep/test.js
+++ counting-sheep/test.js
@@ -5,4 +5,4 @@ var array1 = [true,  true,  true,  false,
               true,  true,  true,  true ,
               false, false, true,  true ];
 
-countSheeps(array1) //Should return 7
+countSheeps(array1) //Should return 17
