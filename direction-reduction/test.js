e1a2747cd479007ff36386dbaca1063c8708d8f5
Naman Sood <54349762+nanusud@users.noreply.github.com>
2020-03-11 20:43:16 +0530

    Create test.js

---
+++ direction-reduction/test.js
@@ -0,0 +1,2 @@
+dirReduc(["NORTH", "SOUTH", "SOUTH", "EAST", "WEST", "NORTH", "WEST"]); // Should return ["WEST"]
+dirReduc(["NORTH", "WEST", "SOUTH", "EAST"]); // Should return ["NORTH", "WEST", "SOUTH", "EAST"]
